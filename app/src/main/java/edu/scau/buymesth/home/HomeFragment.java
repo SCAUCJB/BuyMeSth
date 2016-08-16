d56af8f516a7e8de9bf7e67aefbf491762c06563
JammyR <13680808015@163.com>
2016-08-16 20:30:07 +0800

    更新了HomeFragment的上下滑隐藏Fab

--- app/src/main/java/edu/scau/buymesth/home/HomeFragment.java
+++ app/src/main/java/edu/scau/buymesth/home/HomeFragment.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Toast;
 
 import java.util.List;
@@ -21,6 +23,7 @@ import edu.scau.buymesth.R;
 import edu.scau.buymesth.adapter.HomeAdapter;
 import edu.scau.buymesth.data.bean.Request;
 import edu.scau.buymesth.homedetail.HomeDetailActivity;
+import edu.scau.buymesth.publish.PublishActivity;
 import in.srain.cube.views.ptr.PtrFrameLayout;
 import in.srain.cube.views.ptr.PtrHandler;
 import in.srain.cube.views.ptr.header.StoreHouseHeader;
@@ -50,10 +53,24 @@ public class HomeFragment extends Fragment implements HomeContract.View{
         mPresenter=new HomePresenter();
         mPresenter.setVM(this,new HomeModel());
 
+        mRecyclerView.setOnScrollListener(new HidingScrollListener() {
+            @Override
+            public void onHide() {
+//                fbAdd.setVisibility(View.GONE);
+                fbAdd.animate().translationY(fbAdd.getHeight()+ fbAdd.getBottom()).setInterpolator(new AccelerateInterpolator(2)).start();
+            }
+
+            @Override
+            public void onShow() {
+//                fbAdd.setVisibility(View.VISIBLE);
+                fbAdd.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2)).start();
+            }
+        });
+
         fbAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(),AddActivity.class);
+                Intent intent = new Intent(getContext(),PublishActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/edu/scau/buymesth/publish/PublishContract.java
@@ -0,0 +1,7 @@
+package edu.scau.buymesth.publish;
+
+/**
+ * Created by Jammy on 2016/8/16.
+ */
+public class PublishContract {
+}
