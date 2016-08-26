bdca9c860282d63627e909de3978d0b36a0fe612
JammyR <13680808015@163.com>
2016-08-26 22:42:08 +0800

    更新了上传的RV的删除功能

--- app/src/main/java/edu/scau/buymesth/App.java
+++ app/src/main/java/edu/scau/buymesth/App.java
@@ -1,20 +1,17 @@
 package edu.scau.buymesth;
 
 import android.app.Application;
+import android.graphics.Color;
 
 import com.squareup.leakcanary.LeakCanary;
 
 import cn.bmob.v3.Bmob;
-import cn.bmob.v3.BmobConfig;
-import cn.bmob.v3.BmobUser;
 import cn.finalteam.galleryfinal.CoreConfig;
 import cn.finalteam.galleryfinal.FunctionConfig;
 import cn.finalteam.galleryfinal.GalleryFinal;
-import cn.finalteam.galleryfinal.ImageLoader;
 import cn.finalteam.galleryfinal.ThemeConfig;
 import crush.CustomActivityOnCrash;
 import edu.scau.buymesth.publish.GlideImageLoader;
-import edu.scau.buymesth.ui.WelcomeActivity;
 
 /**
  * Created by John on 2016/8/4.
@@ -28,18 +25,21 @@ public class App extends Application {
         LeakCanary.install(this);
         //在这里进行初始化的操作
         Bmob.initialize(this, "211614edac96ab4c9492179cf459993a");
+        initGallery();
+    }
 
-        //设置主题
-        //ThemeConfig.CYAN
+    private void initGallery() {
+
+        //设置相册主题
         ThemeConfig theme = new ThemeConfig.Builder()
-//                .setTitleBarBgColor()
-                .setIconCamera(R.mipmap.ic_camear)
+                .setTitleBarBgColor(Color.rgb(66, 66, 66))
+                .setFabNornalColor(Color.rgb(66, 66, 66))
+                .setIconCamera(R.mipmap.ic_camera_alt_white_24dp)
                 .build();
         //配置功能
         FunctionConfig functionConfig = new FunctionConfig.Builder()
                 .setEnableCamera(true)
                 .build();
-
         //配置imageloader
         GlideImageLoader imageloader = new GlideImageLoader();
         CoreConfig coreConfig = new CoreConfig.Builder(this, imageloader, theme)
--- app/src/main/java/edu/scau/buymesth/adapter/PictureAdapter.java
+++ app/src/main/java/edu/scau/buymesth/adapter/PictureAdapter.java
@@ -1,6 +1,10 @@
 package edu.scau.buymesth.adapter;
 
 
+import android.media.Image;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -28,17 +32,30 @@ public class PictureAdapter extends BaseQuickAdapter<PhotoInfo> {
 
     /////布局和数据
     public PictureAdapter(List<PhotoInfo> data) {
-        super(R.layout.picture_item, data);
+        super(R.layout.item_picture, data);
         setList(data);
     }
 
     @Override
     protected void convert(BaseViewHolder helper, PhotoInfo item) {
         if (helper.getLayoutPosition()==getItemCount()-1&&getItemCount()!=9) {
+            ImageView btn = helper.getView(R.id.btn_cancle);
+            btn.setVisibility(View.GONE);
             Glide.with(mContext).load(R.mipmap.ic_add).centerCrop().into((ImageView) helper.getView(R.id.iv));
         } else {
             String url = item.getPhotoPath();
             Glide.with(mContext).load(url).centerCrop().into((ImageView) helper.getView(R.id.iv));
+            ImageView btn = helper.getView(R.id.btn_cancle);
+            btn.setVisibility(View.VISIBLE);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //TODO:如果满9个删除的特殊情况还没考虑
+                    Log.v("点击了","删除了");
+                    getData().remove(helper.getPosition());
+                    notifyDataSetChanged();
+                }
+            });
         }
     }
 
