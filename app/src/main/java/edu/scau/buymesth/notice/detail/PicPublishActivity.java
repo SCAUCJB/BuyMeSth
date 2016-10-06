2b729ecaf726c89c602f0e9290546156bba91367
JammyR <13680808015@163.com>
2016-10-06 18:19:07 +0800

    更新了图片选择功能

--- app/src/main/java/edu/scau/buymesth/notice/detail/PicPublishActivity.java
+++ app/src/main/java/edu/scau/buymesth/notice/detail/PicPublishActivity.java
@@ -1,149 +1,194 @@
 package edu.scau.buymesth.notice.detail;
 
-import android.app.ProgressDialog;
+
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
 
 import base.BaseActivity;
 import base.util.SpaceItemDecoration;
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.datatype.BmobFile;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.QueryListener;
 import cn.bmob.v3.listener.SaveListener;
 import cn.bmob.v3.listener.UploadBatchListener;
 import edu.scau.buymesth.R;
-import edu.scau.buymesth.notice.PicAdapter;
+import edu.scau.buymesth.adapter.MyPictureAdapter;
 import edu.scau.buymesth.data.bean.Order;
 import edu.scau.buymesth.data.bean.OrderMoment;
+import edu.scau.buymesth.data.bean.Request;
 import edu.scau.buymesth.util.CompressHelper;
 import gallery.PhotoActivity;
 import me.iwf.photopicker.PhotoPicker;
+import rx.Observable;
+import util.FileUtils;
 
-import static java.util.concurrent.Executors.newFixedThreadPool;
-
-/**
- * Created by Jammy on 2016/10/1.
- */
 public class PicPublishActivity extends BaseActivity {
+
+
+    ArrayList<MyPictureAdapter.ImageItem> mUrlList;
+    MyPictureAdapter adapter;
+    boolean mCompressing = false;
+    boolean mCompressed = false;
+    boolean mCompress = false;
+    long mImageSize = 0;
+    @Bind(R.id.btn_commit)
+    Button btnCommit;
     @Bind(R.id.et)
     EditText et;
     @Bind(R.id.rv)
-    RecyclerView mRecyclerView;
-    PicAdapter adapter;
-    List<String> list = new ArrayList<>();
-    public ArrayList<String> mUrlList = new ArrayList<>(9);
-    @Bind(R.id.btn_commit)
-    Button btnCommit;
-    private volatile ArrayList<String> mCompressList = new ArrayList<>(9);
-    public volatile boolean mCompressing;
-    ExecutorService threadPoolExecutor = newFixedThreadPool(1);
-    List<String> picWidths = new LinkedList<>();
-    List<String> picHeights = new LinkedList<>();
-    private ProgressDialog mDialog;
+    RecyclerView recyclerView;
+    @Bind(R.id.sw_compress)
+    Switch swCompress;
+    @Bind(R.id.tv_size)
+    TextView tvSize;
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_pic_publish;
     }
-    public void showLoadingDialog() {
-        if (mDialog == null) {
-            mDialog = new ProgressDialog(mContext);
-            mDialog.setCancelable(false);
-            mDialog.setMessage("请稍等");
-            mDialog.setMax(100);
-            mDialog.setProgressStyle(1);
-        }
-        mDialog.show();
-    }
-    public void setProgress(Integer progress){
-        mDialog.setProgress(progress);
-    }
+
     @Override
     public void initView() {
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
-        mRecyclerView.setLayoutManager(gridLayoutManager);
-        mRecyclerView.addItemDecoration(new SpaceItemDecoration(getResources().getDimensionPixelSize(R.dimen.dp_6)));
-        //    mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setNestedScrollingEnabled(false);
-        adapter = new PicAdapter(list);
+        mUrlList = new ArrayList<>();
 
-        mRecyclerView.setAdapter(adapter);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.addItemDecoration(new SpaceItemDecoration(getResources().getDimensionPixelSize(R.dimen.dp_6)));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setNestedScrollingEnabled(false);
+        adapter = new MyPictureAdapter(mUrlList);
+        recyclerView.setAdapter(adapter);
         adapter.setOnRecyclerViewItemClickListener((view, position) -> {
             ////这里设置点击事件
+            ArrayList<String> selectImages = new ArrayList<>();
             if (adapter.getItemId(position) == 1) {
+                for (MyPictureAdapter.ImageItem ii : mUrlList) selectImages.add(ii.sourceImage);
                 PhotoPicker.builder()
                         .setPhotoCount(9)
                         .setShowCamera(true)
                         .setShowGif(false)
                         .setPreviewEnabled(true)
-                        .setSelected(mUrlList)
+                        .setSelected(selectImages)
                         .start(PicPublishActivity.this, PhotoPicker.REQUEST_CODE);
             } else {
-                PhotoActivity.navigate(PicPublishActivity.this, view, adapter.getItem(position), position);
+                for (MyPictureAdapter.ImageItem ii : mUrlList)
+                    selectImages.add(mCompress ? ii.compressedImage : ii.sourceImage);
+                PhotoActivity.navigate(PicPublishActivity.this, recyclerView, selectImages, position);
             }
         });
-        btnCommit.setOnClickListener(v -> {
-            showLoadingDialog();
-            if(mCompressList.get(mCompressList.size()-1)==null)
-            mCompressList.remove(mCompressList.size()-1);
-            String[] fileList = new String[mCompressList.size()];
-            mCompressList.toArray(fileList);
-            BmobFile.uploadBatch(fileList, new UploadBatchListener() {
-                @Override
-                public void onSuccess(List<BmobFile> list, List<String> urls) {
-                    if (urls.size() == fileList.length) {//如果数量相等，则代表文件全部上传完成
-                        OrderMoment orderMonent = new OrderMoment();
-                        orderMonent.setText(et.getText().toString());
-                        orderMonent.setPicList(urls);
-                        orderMonent.setOrder((Order) getIntent().getSerializableExtra("order"));
-                        orderMonent.save(new SaveListener<String>() {
-                            @Override
-                            public void done(String s, BmobException e) {
-                                if (e == null) {
-                                    closeLoadingDialog();
-                                    Toast.makeText(PicPublishActivity.this,"上传成功",Toast.LENGTH_LONG).show();
-                                    finish();
-                                } else {
-                                    Log.e("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
-                                    closeLoadingDialog();
-                                }
-                            }
-                        });
-                    }
-                }
 
-                @Override
-                public void onProgress(int curIndex, int curPercent, int total, int totalPercent) {
-                    //1、curIndex--表示当前第几个文件正在上传
-                    //2、curPercent--表示当前上传文件的进度值（百分比）
-                    //3、total--表示总的上传文件数
-                    //4、totalPercent--表示总的上传进度（百分比）
-                    setProgress(totalPercent);
-                }
-
-                @Override
-                public void onError(int i, String s) {
-
-                }
-            });
+        swCompress.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            mCompress = isChecked;
+            if (isChecked && !mCompressed && !mCompressing) {
+                compress();
+            } else if (mCompressed && !mCompressing) {
+                mImageSize = 0;
+                adapter.setList(mUrlList, mCompress ? 1 : 0);
+                Observable.from(mUrlList)
+                        .map(imageItem -> new File(mCompress ? imageItem.compressedImage : imageItem.sourceImage))
+                        .subscribe(file -> mImageSize += file.length(),
+                                o -> {
+                                },
+                                () -> tvSize.setText("图片大小："+ FileUtils.convert(mImageSize)));
+            }
         });
-    }
 
-    @Override
-    public boolean canSwipeBack() {
-        return true;
+
+        btnCommit.setOnClickListener(v -> {
+            //upload images
+            if (mUrlList.size() > 0) {
+                if (mCompressing) {
+                    toast("压缩中");
+                    return;
+                }
+                btnCommit.setEnabled(false);
+                ArrayList<String> selectImages = new ArrayList<>();
+                for (MyPictureAdapter.ImageItem ii : mUrlList)
+                    selectImages.add(mCompress ? ii.compressedImage : ii.sourceImage);
+                String[] sendList = new String[selectImages.size()];
+                selectImages.toArray(sendList);
+
+                BmobFile.uploadBatch(sendList, new UploadBatchListener() {
+                    @Override
+                    public void onSuccess(List<BmobFile> files, List<String> urls) {
+                        if (urls.size() >= mUrlList.size()) {
+                            OrderMoment orderMonent = new OrderMoment();
+                            orderMonent.setText(et.getText().toString());
+                            orderMonent.setPicList(urls);
+                            orderMonent.setOrder((Order) getIntent().getSerializableExtra("order"));
+                            orderMonent.save(new SaveListener<String>() {
+                                @Override
+                                public void done(String s, BmobException e) {
+                                    if (e == null) {
+                                        closeLoadingDialog();
+                                        Toast.makeText(PicPublishActivity.this, "上传成功", Toast.LENGTH_LONG).show();
+                                        finish();
+                                    } else {
+                                        Log.e("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
+                                        closeLoadingDialog();
+                                    }
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onProgress(int curIndex, int curPercent, int total, int totalPercent) {
+                        //1、curIndex--表示当前第几个文件正在上传
+                        //2、curPercent--表示当前上传文件的进度值（百分比）
+                        //3、total--表示总的上传文件数
+                        //4、totalPercent--表示总的上传进度（百分比）
+                        View view = recyclerView.getChildAt(curIndex - 1);
+                        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.progress_upload);
+                        if (progressBar != null) {
+                            progressBar.setVisibility(View.VISIBLE);
+                            progressBar.setMax(100);
+                            progressBar.setProgress(curPercent);
+                        }
+                    }
+
+                    @Override
+                    public void onError(int i, String s) {
+                        toast(s);
+                    }
+                });
+            } else {
+                OrderMoment orderMonent = new OrderMoment();
+                orderMonent.setText(et.getText().toString());
+                orderMonent.setOrder((Order) getIntent().getSerializableExtra("order"));
+                orderMonent.save(new SaveListener<String>() {
+                    @Override
+                    public void done(String s, BmobException e) {
+                        if (e == null) {
+                            closeLoadingDialog();
+                            Toast.makeText(PicPublishActivity.this, "上传成功", Toast.LENGTH_LONG).show();
+                            finish();
+                        } else {
+                            Log.e("bmob", "失败：" + e.getMessage() + "," + e.getErrorCode());
+                            closeLoadingDialog();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -152,46 +197,76 @@ public class PicPublishActivity extends BaseActivity {
 
         if (resultCode == RESULT_OK && requestCode == PhotoPicker.REQUEST_CODE) {
             if (data != null) {
-                mCompressList = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
-                Log.d("zhx", "size=" + mCompressList.size());
-                for (int i = 0; i < mCompressList.size(); ++i)
-                    mUrlList.add(i, mCompressList.get(i));
-                adapter.setList(mCompressList);
-                toast("开始压缩图片");
-                new Thread(this::compress).start();
+                ArrayList<String> photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS);
+                mUrlList.clear();
+                for (String url : photos) {
+                    mUrlList.add(new MyPictureAdapter.ImageItem(url, null));
+                }
+                adapter.setList(mUrlList, mCompress ? 1 : 0);
+                mImageSize = 0;
+                mCompressed = false;
+                Observable.from(mUrlList)
+                        .map(imageItem -> new File(mCompress ? imageItem.compressedImage : imageItem.sourceImage))
+                        .subscribe(file -> mImageSize += file.length(),
+                                o -> {
+                                },
+                                () -> tvSize.setText("图片大小："+ FileUtils.convert(mImageSize)));
+                if (mCompress) compress();
             }
         }
     }
 
     private void compress() {
-        mCompressing = true;
-        CompressHelper compressHelper = new CompressHelper(mContext);
-        //判断最后一个元素是否包含空
-        int count = mCompressList.get(mCompressList.size() - 1) != null ? mCompressList.size() : mCompressList.size() - 1;
-        CountDownLatch countDownLatch = new CountDownLatch(count);
-        for (int i = 0; i < count; i++) {
-            final int finalI = i;
-            threadPoolExecutor.execute(() -> {
-                compressHelper.setFilename("cc_" + finalI);
-                compressHelper.setWidthList(picWidths);
-                compressHelper.setHeightList(picHeights);
-                mCompressList.set(finalI, compressHelper.thirdCompress(new File(mCompressList.get(finalI))));
-                countDownLatch.countDown();
-            });
-        }
-        try {
-            countDownLatch.await();
-            runOnUiThread(() -> {
-                toast("压缩完成");
-                //因为会自动加上一个NULL，这里要把之前被加上的null去除。。。
-                if (mCompressList.size() != 9)
-                    mCompressList.remove(mCompressList.size() - 1);
-                adapter.setList(mCompressList);
-            });
-            mCompressing = false;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        swCompress.setEnabled(false);
+        tvSize.setText("压缩中");
+        new Thread(() -> {
+            mCompressing = true;
+            CompressHelper compressHelper = new CompressHelper(mContext);
+            CountDownLatch countDownLatch = new CountDownLatch(mUrlList.size());
+            for (int i = 0; i < mUrlList.size(); i++) {
+                final int finalI = i;
+                new Thread(() -> {
+                    compressHelper.setFilename("cc_" + finalI);
+//                    mUrlList.set(finalI,compressHelper.thirdCompress(new File(mUrlList.get(finalI))));
+                    mUrlList.get(finalI).compressedImage = compressHelper.thirdCompress(new File(mUrlList.get(finalI).sourceImage));
+                    countDownLatch.countDown();
+                }).start();
+            }
+            try {
+                countDownLatch.await();
+                runOnUiThread(() -> {
+                    toast("压缩完成");
+                    mImageSize = 0;
+                    Observable.from(mUrlList)
+                            .map(imageItem -> new File(mCompress ? imageItem.compressedImage : imageItem.sourceImage))
+                            .subscribe(file -> mImageSize += file.length(),
+                                    o -> {
+                                    },
+                                    () -> tvSize.setText("图片大小："+ FileUtils.convert(mImageSize)));
+                    adapter.setList(mUrlList, mCompress ? 1 : 0);
+                    swCompress.setEnabled(true);
+                });
+                mCompressing = false;
+                mCompressed = true;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    @Override
+    public boolean canSwipeBack() {
+        return true;
+    }
+
+    @Override
+    public boolean showColorStatusBar() {
+        return true;
+    }
+
+    @Override
+    public int getStatusColorResources() {
+        return R.color.colorPrimaryDark;
     }
 
     @Override
